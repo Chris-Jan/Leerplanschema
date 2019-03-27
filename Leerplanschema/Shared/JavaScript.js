57e8159e71f5a4379b3d738fac694c5157571eb0
Unknown <chris-janborgers@hotmail.com>
2019-03-27 22:42:52 +0100

    wip

--- Leerplanschema/Shared/JavaScript.js
+++ Leerplanschema/Shared/JavaScript.js
@@ -2,3 +2,12 @@
     window.location = $(this).find("a").attr("href");
     return false;
 });
+
+$("[class=btnModalPopup]").live("click", function () {
+    $("#modal_dialog").dialog({
+        title: "jQuery Modal Dialog Popup",
+
+
+        modal: true
+    });
+    return false;
